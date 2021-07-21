d7c823c3b54bb56dac48dc7b8596808317afa790
anthony klein <netflixsucks179@gmail.com>
2021-07-21 14:53:09 -0700

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+      <header>
+      <div class="container"> 
+        <div class="nav">
+          <div class="nav1"></div>
+          <div class="nav2">
+            <div class="menu"><a href="#about">about Me</a></div>
+            <div class="menu"><a href="#services">services</a></div>
+            <div class="menu"><a href="#">Blogs</a></div>
+            <div class="menu">
+              <a href="./template/portifolio.html">Portifolio</a>
+            </div>
+            <div class="menu"><a href="#contact">contact Me</a></div>
+          </div>
+        
+        </div>
+      </div>
+    </header> 
+    </>
+  );
+}
+
+export default App;
